561888c6f04ea822c5b93cf782908753504471a5
rubin-khadka <rubin.robotic@gmail.com>
2025-11-29 15:09:31 +0100

    implemented LM35 temperature mode using ASC1 IN0 pin

---
+++ Core/Inc/temp_adc.h
@@ -0,0 +1,46 @@
+/**
+  * @file    temp_adc.h
+  * @brief   Temperature ADC module for STM32F103 using LM35 sensor
+  * @author  Rubin Khadka
+  */
+	
+#ifndef TEMP_ADC_H
+#define TEMP_ADC_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Include Definitions */
+#include "main.h"
+
+/* Defines */
+#define TEMP_ADC_CHANNEL 				0x00		// ADC Channel 0 (PA0)
+#define TEMP_ADC_SAMPLING_TIME 	0x03		// 28.5 sampling cycle
+
+// Temperature calculation
+#define VREF    								3.3f 		// 3.3V reference in mV
+#define ADC_RESOLUTION      		4095.0f // 12-bit ADC
+#define LM35_SCALE_FACTOR     	100.0f 	// LM35: 10mV/°C = 0.01V/°C × 100
+
+// Integer-based constants
+#define VREF_MV 3300        // 3.3V in millivolts
+#define ADC_RESOLUTION_INT 4095 // 12-bit ADC
+#define VIRTUAL_GROUND_MV 1650  // 1.65V in millivolts
+
+/* Global Variables */
+extern volatile uint32_t adc_raw_value;
+extern volatile uint8_t adc_conversion_complete;
+
+/* Function Prototypes */
+void temp_adc_init(void);
+void temp_adc_start_conversion(void);
+uint8_t temp_adc_is_ready(void);
+float temp_adc_read_celsius(void);
+void ADC1_2_IRQHandler(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* TEMP_ADC_H */
---
+++ Core/Src/temp_adc.c
@@ -0,0 +1,82 @@
+/**
+  * @file    temp_adc.c
+  * @brief   Temperature ADC module - Register level implementation
+  * @author  Rubin Khadka
+  */
+	
+#include "temp_adc.h"
+
+/* Global Variables */
+volatile uint32_t adc_raw_value = 0;
+volatile uint8_t adc_conversion_complete = 0;
+
+/* Initialize ADC1 Channel 0 for temperature measurement */
+void temp_adc_init(void)
+{
+	// Enable ADC1 clock
+	RCC->APB2ENR |= RCC_APB2ENR_ADC1EN;
+	
+	// Power on ADC
+	ADC1->CR2 |= ADC_CR2_ADON;
+	
+	// Configure Sampling time for Channel 0
+	ADC1->SMPR2 &= ~(0x07 << 0);		// Clear existing sampling cycle
+	ADC1->SMPR2 |= (TEMP_ADC_SAMPLING_TIME << 0);		// Set sampling cycle 28.5 
+	
+	// Configure conversion sequence 
+	ADC1->SQR1 &= ~(0x0F << 20);   	// 1 conversion in sequence
+	ADC1->SQR3 = TEMP_ADC_CHANNEL;  // SQ1[4:0] = Channel 0
+	
+	// ADC calibration 
+	ADC1->CR2 |= ADC_CR2_RSTCAL;
+	while (ADC1->CR2 & ADC_CR2_RSTCAL);
+	
+	ADC1->CR2 |= ADC_CR2_CAL;
+	while (ADC1->CR2 & ADC_CR2_CAL);
+	
+	// Final configuration
+	ADC1->CR2 &= ~ADC_CR2_CONT;  // Single conversion mode
+	ADC1->CR2 &= ~ADC_CR2_ALIGN; // Right alignment
+	
+	// Enable ADC interrupt in NVIC
+	ADC1->CR1 |= ADC_CR1_EOCIE;
+	NVIC_EnableIRQ(ADC1_2_IRQn);   
+		
+	// Clear any previous values 
+	adc_raw_value = 0;
+	adc_conversion_complete = 0;
+	
+	// Brief stabilization delay
+	for (volatile uint32_t i = 0; i < 100000; i++);  // Approx 1ms delay
+}
+
+void temp_adc_start_conversion(void)
+{
+	adc_conversion_complete = 0;
+	ADC1->CR2 |= (0x01 << 22);
+//	ADC1->CR2 |= ADC_CR2_SWSTART;
+}
+
+uint8_t temp_adc_is_ready(void)
+{
+	return adc_conversion_complete;
+}
+
+float temp_adc_read_celsius(void)
+{
+    float voltage = (adc_raw_value * VREF) / ADC_RESOLUTION;
+		float temperature = (voltage - 1.65f) * LM35_SCALE_FACTOR;
+    return temperature;
+}
+
+void ADC1_2_IRQHandler(void)
+{
+	if (ADC1->SR & ADC_SR_EOC)
+	{
+		adc_raw_value = ADC1->DR;
+		adc_conversion_complete = 1;
+		ADC1->SR &= ~ADC_SR_EOC;
+	}
+}
+
+
